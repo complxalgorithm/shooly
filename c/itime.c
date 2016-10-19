6058f33748039681353df483b306afec0db032dd
Stephen C. Sanders <scsandman6496@gmail.com>
2016-10-19 17:58:34 -0400

    fixed header file

--- c/itime.c
+++ c/itime.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <signal.h>
-#include <cctype.h>
+#include <ctype.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>   /* for PF_INET sockets */
